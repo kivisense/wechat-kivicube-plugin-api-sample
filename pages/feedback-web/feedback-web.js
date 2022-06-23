c94dc50d4a5908b784eaea0c4ca1f24c76667ba6
蓝宏金 <honkinlan@163.com>
2022-06-23 11:20:23 +0800

    update: webview反馈页传入设备性能等级并加入loading

--- pages/feedback-web/feedback-web.js
+++ pages/feedback-web/feedback-web.js
@@ -2,22 +2,35 @@
 Page({
   data: {
     statusBarHeight: 0,
-    title: ''
+    title: "",
+    webviewUrl: ""
   },
-  onLoad: function () {
+  
+  onLoad() {
     // 客户端 6.7.2 版本开始，navigationStyle: custom 对 web-view 组件无效
     // const sceneData = wx.getStorageSync("sceneData");
     // this.setData({ title: sceneData.title });
     // wx.setNavigationBarTitle({
     //   title: sceneData.title
     // })
+    wx.showLoading({ title: "加载中" });
+
+    const { benchmarkLevel } = wx.getSystemInfoSync();
+    const webviewUrl = `https://project.kivisense.com/helper-tools/feedback.html?benchmarkLevel=${benchmarkLevel}`;
+    this.setData({ webviewUrl });
+    
     wx.getSystemInfoAsync({
       success: res => {
         this.setData({statusBarHeight: res.statusBarHeight})
       }
     })
   },
-  handleBack(){
+
+  webviewLoad() {
+    wx.hideLoading();
+  },
+
+  handleBack() {
     const pagesArr = getCurrentPages()
     if(pagesArr.length === 1) {
       wx.reLaunch({
@@ -27,7 +40,8 @@ Page({
       wx.navigateBack();
     }
   },
-  onShareAppMessage: function () {
+
+  onShareAppMessage() {
     return {
       title: "Kivicube企业版高级API示例 - WebView反馈",
       path: "/pages/feedback-web/feedback-web",
