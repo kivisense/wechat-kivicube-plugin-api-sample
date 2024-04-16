66ba3507926a51eaaf0219a820b994ce1546014c
evo.wang <648977787@qq.com>
2024-04-16 18:48:53 +0800

    feat: 兼容反馈页面修改

--- pages/tools/miniprogram-feedback/miniprogram-feedback.js
+++ pages/tools/miniprogram-feedback/miniprogram-feedback.js
@@ -5,79 +5,95 @@ const {
   deviceSupportVerticalPlane,
   deviceSupportWorld,
 } = requirePlugin("kivicube");
-
+const LABELS = [
+  { key: "marker-ar", name: "图像AR" },
+  { key: "cloudar", name: "云识别/陀螺仪" },
+  { key: "show3d", name: "3D互动" },
+  { key: "body-ar", name: "身体AR" },
+  { key: "plane", name: "平面AR" },
+  { key: "roam", name: "漫游AR" },
+  { key: "world", name: "世界AR" },
+  { key: "landmark", name: "地标AR" },
+];
 Page({
   data: {
     list: [],
   },
   onLoad() {
-    const supportV1 = deviceSupportV1();
-    const supportV2 = deviceSupportV2();
-    const supportVerticalPlane = deviceSupportVerticalPlane();
-    const supportWorld = deviceSupportWorld();
-    console.log(`supportV1`, supportV1);
-    console.log(`supportV2`, supportV2);
-    console.log(`supportVerticalPlane`, supportVerticalPlane);
-    console.log(`supportWorld`, supportWorld);
-
-    const textIdx = (() => {
-      if (!supportV1 && !supportV2) return 0;
-      if (supportV2) {
-        if (supportVerticalPlane) {
-          return 3;
-        }
-        return 2;
+    let list = LABELS.map((label) => {
+      let item = {
+        label: label.name,
+        value: "",
+      };
+      switch (label.key) {
+        case "marker-ar":
+          if (this.isVersionOver("2.25.0")) {
+            item.value = "支持V1，V2";
+          } else if (this.isVersionOver("2.14.0")) {
+            item.value = "支持V1";
+          } else {
+            item.value = "不支持";
+          }
+          break;
+        case "cloudar":
+          if (this.isVersionOver("2.14.0")) {
+            item.value = "支持";
+          } else {
+            item.value = "不支持";
+          }
+          break;
+        case "show3d":
+          if (this.isVersionOver("2.14.0")) {
+            item.value = "支持";
+          } else {
+            item.value = "不支持";
+          }
+          break;
+        case "body-ar":
+          item.value = "不支持，可以通过WebView支持";
+          break;
+        case "plane":
+          if (deviceSupportVerticalPlane()) {
+            item.value = "支持，V1，V2(水平+竖直面)";
+          } else if (deviceSupportV2()) {
+            item.value = "支持，V1，V2(水平面)";
+          } else if (deviceSupportV1()) {
+            item.value = "支持，V1";
+          } else {
+            item.value = "不支持";
+          }
+          break;
+        case "roam":
+          if (deviceSupportV2()) {
+            item.value = "支持";
+          } else if (deviceSupportV1()) {
+            item.value = "支持, 兼容模式";
+          } else {
+            item.value = "不支持";
+          }
+          break;
+        case "world":
+          if (deviceSupportWorld()) {
+            item.value = "支持";
+          } else {
+            item.value = "不支持";
+          }
+          break;
+        case "landmark":
+          if (this.isVersionOver("2.25.0")) {
+            item.value = "支持";
+          } else {
+            item.value = "不支持";
+          }
+          break;
       }
-      if (supportV1) return 1;
-    })();
-
-    let list = [
-      {
-        label: "图像AR",
-        value: `${
-          ["不支持", "支持，V1", "支持，V1，V2", "支持，V1，V2"][textIdx]
-        }`,
-      },
-      {
-        label: "云识别/陀螺仪",
-        value: `支持`,
-      },
-      {
-        label: "3D互动",
-        value: `支持`,
-      },
-      {
-        label: "身体AR",
-        value: `不支持，可以通过WebView支持`,
-      },
-      {
-        label: "平面AR",
-        value: `${
-          [
-            "不支持",
-            "支持，V1",
-            "支持，V1，V2(水平面)",
-            "支持，V1，V2(水平+竖直面)",
-          ][textIdx]
-        }`,
-      },
-      {
-        label: "漫游AR",
-        value: `${["不支持", "支持，兼容模式", "支持", "支持"][textIdx]}`,
-      },
-      {
-        label: "世界AR",
-        value: `${supportWorld ? "支持" : "不支持"}`,
-      },
-      {
-        label: "地标AR",
-        value: `${["不支持", "不支持", "支持", "支持"][textIdx]}`,
-      },
-    ];
+      return item;
+    });
     this.setData({
       list,
     });
   },
+  isVersionOver(v) {},
   onShareAppMessage: function () {
     return {
       title: "Kivicube企业版高级API示例 - 小程序兼容反馈",
